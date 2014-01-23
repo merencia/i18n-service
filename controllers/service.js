351b85b5852a7e66f3f5ed4894edb56dd6a4007f
Lucas Jose Merencia <lucas.merencia@gmail.com>
2014-01-23 16:18:15 -0200

    wip

--- controllers/service.js
+++ controllers/service.js
@@ -1,27 +1,51 @@
 module.exports = function(app) {
-
+ /**
+  * Resolves literals passed as parameter
+  *
+  * @param <Object> request
+  *
+  * @return <Array> a array with literals  
+  */
   var getLiterals = function(req){
     literals = req.body.literals || req.query.literals
-    if (literals)
-      return literals.split(',')
+    return literals ? literals.split(',') : [] 
   }
 
+ /**
+  * Translata a array of literals
+  *
+  * @param <Array> literals to translate
+  * @param <Object> a object with literals and translations
+  *
+  * @return <Object> a object with translations
+  */
   var translate = function(literals, locale){
     translated = {}
 
-    for (i in literals) {
-      literal = literals[i]
-      translated[literal] = find(literal, locale)
+    if (literals.length > 0){
+      for (i in literals) {
+        literal = literals[i]
+        translated[literal] = find(literal, locale)
+      }
+    }else{
+       translated = locale
     }
-
     return translated
   }
 
+ /**
+  * Find a literal in locale object
+  * 
+  * @param <String> literal 
+  * @param <Object> a object with literals and translations
+  *
+  * @return <Object> a object with translations
+  */
   var find = function(literal, locale){
     index = literal.indexOf('.')
     if(index != -1){
       parent = literal.substring(0, index)
-      return find(literal.substring(index), locale[parent])
+      return find(literal.substring(index + 1), locale[parent])
     }else {
       return locale[literal]
     }
@@ -29,20 +53,16 @@ module.exports = function(app) {
 
   var ServiceController = {
     translate: function(req, res) {
-      
+
       localeName = req.params.locale
-      locale = app.locales[localeName]
+      if(app.locales.hasOwnProperty(localeName)){
+        locale = app.locales[localeName]
 
-      literals = getLiterals(req)
-      if (literals){
+        literals = getLiterals(req)
         translated = translate(literals, locale)
-      } else {
-        translated = locale
-      }
 
-      contentType = {"Content-Type": "application/json; charset=utf-8"}
+        contentType = {"Content-Type": "application/json; charset=utf-8"}
 
-      if(translated){
         res.writeHead(200, contentType)
         res.write(JSON.stringify(translated), 'utf8')
       }else {
