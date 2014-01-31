28df64112e1105f606d5aeb74b20aacb30152949
Lucas Jose Merencia <lucas.merencia@gmail.com>
2014-01-31 14:04:07 -0200

    removing express-load...

--- i18n-service.js
+++ i18n-service.js
@@ -3,16 +3,14 @@
 var express = require('express'),
     load    = require('express-load'), 
     fs      = require('fs'), 
-    app     = express()
+    app     = express(),
+    service = require('./controllers/service'),
+    route   = require('./routes/service')
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(app.router);
-
-load('controllers')
-  .then('routes')
-  .into(app);
 
 if (process.env.LOCALES_FOLDER){
   localesFolder = process.env.LOCALES_FOLDER;
@@ -29,9 +27,20 @@ if (process.env.LOCALES_FOLDER){
       app.locales[key] = locale[key];
     }
   }
+
+  console.log(app.locales)
   
+  console.log("loadding service...")
+  app.controllers = {
+    service : service(app)
+  }
+
+  console.log("loadding routes...")
+  route(app);
+
+
   app.listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
+    console.log('I18n service listening on port ' + app.get('port'));
   });
 
   module.exports = app;
